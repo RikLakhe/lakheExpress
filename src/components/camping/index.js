99727a5eac199bd867a7ff20877337cb838f2f1c
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-08-10 00:52:12 +0545

    added camping project finished

--- src/components/camping/index.js
+++ src/components/camping/index.js
@@ -16,5 +16,24 @@ router.get('/',
     },
     responseHandler);
 
+router.get('/info/:id',
+    init,
+    (req, res, next) => {
+        let id = req.params.id
+        res.locals.status = 200;
+        res.locals.data = campingDetail[id].info
+        next();
+    },
+    responseHandler);
+
+router.get('/item/:id',
+    init,
+    (req, res, next) => {
+        let id = req.params.id
+        res.locals.status = 200;
+        res.locals.data = campingDetail[id].item
+        next();
+    },
+    responseHandler);
 
 module.exports = router;
